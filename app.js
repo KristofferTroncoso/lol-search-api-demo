45f6852411e7fdc7194ec00404d84dc70e83dd10
HarryPirate <greenorange@hellokitty.com>
2018-03-02 08:04:48 +0000

    Add summoner search functionality to homepage
    Added form to homepage to allow search for summoner by summoner
    name. NPM package body-parser was installed

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const app = express();
 const ejs = require('ejs');
+const bodyParser = require('body-parser');
 const request = require('request');
 const apikey = require('./apikey');
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
     res.render('index');
+});
+
+app.post('/', function(req, res) {
+    var q = req.body.q;
+    res.redirect('/' + q);
 });
 
 app.get('/:summonername', function(req, res) {
